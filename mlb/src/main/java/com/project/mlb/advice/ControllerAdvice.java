7e5120dd2ab439268c0b4c223d09c573847fd158
HiiWee <wpdlzhf159@gmail.com>
2023-04-30 00:32:54 +0900

    feat: 예외에 대한 공통 응답 생성

---
+++ mlb/src/main/java/com/project/mlb/advice/ControllerAdvice.java
@@ -0,0 +1,33 @@
+package com.project.mlb.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(final BindingResult bindingResult) {
+        String message = bindingResult.getFieldErrors()
+                .get(0)
+                .getDefaultMessage();
+        return ResponseEntity.badRequest().body(new ErrorResponse(message));
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(final BadRequestException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.internalServerError().body(new ErrorResponse("서버에서 알 수 없는 오류가 발생했습니다."));
+    }
+
+}
